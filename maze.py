1685855986d8675cc441c4cf32dc6bd21e2aac13
Colby Ackerman <>
2021-04-28 16:56:14 +0000

    light refactoring

--- maze.py
+++ maze.py
@@ -2,15 +2,13 @@ import random, time
 from os import system
 
 # maze dimensions (odd numbers only please)
-width = 77
-height = 35
+width = 25
+height = 15
 
 # primary getcharacter array comprised of cells
 maze = []
 
-wall_character = "█"
-
-# a cell has a position, getstatus, and getcharacter
+# a cell has a position, status, and character
 cell0 = [0, 0, False, "█"]
 
 # holds the current position of the tunneler
@@ -25,17 +23,23 @@ DOWN 	= 1
 LEFT	= 2
 RIGHT	= 3
 
+def numtodirection(num):
+	if num == UP	:  return "UP"
+	if num == DOWN	:  return "DOWN"
+	if num == LEFT	:  return "LEFT"
+	if num == RIGHT	:  return "RIGHT"
 
-def getdirection(dir):
-	if dir == UP: 	return "UP"
-	if dir == DOWN: return "DOWN"
-	if dir == LEFT: return "LEFT"
-	if dir == RIGHT:return "RIGHT"
+# configuration
+SHOW_DEMO = True
+WALL  = "█"
+EMPTY = " "
 
 
-# cell retriever function
-def getcell (row, col):
-	return maze [row] [col]
+# retrieve cell from maze
+def getcell (pos):
+	posrow = pos [0]
+	poscol = pos [1]
+	return maze [posrow] [poscol]
 
 
 # cell attribute accessor functions
@@ -45,17 +49,30 @@ def getrow (cell):
 def getcol (cell):
 	return cell [1]
 
-def getstatus(cell):
+def getvisited(cell):
 	return cell [2]
+
+def setvisited(cell, status):
+	cell [2] = status
 
 def getcharacter(cell):
 	return cell [3]
 
+def setcharacter(cell, c):
+	cell [3] = c
+
+
 
 def outofbounds(row, col):
-	if row < 1 or row >= height - 1 : return True
-	if col < 1 or col >= width - 1: return True
+	# bounds are inset one row and column
+	if row < 1  or  row >= height - 1 : return True
+	if col < 1  or  col >=  width - 1 : return True
 	return False
+
+def removewall(cell):
+	setvisited (cell, True)
+	setcharacter (cell, EMPTY)
+
 
 
 # maze creation function
@@ -66,7 +83,7 @@ def create_maze():
 
 		row_of_cells = []
 		for column in range (width):
-			c = [row, column, False, wall_character]
+			c = [row, column, False, WALL]
 			row_of_cells.append (c)
 	
 		maze.append(row_of_cells)
@@ -90,34 +107,32 @@ def tunnelthemaze():
 	# record the tunnelers starting position
 	tunneler_history.append(tunneler)
 
-	# mark start cell visited
-	t_row = tunneler [0]
-	t_col = tunneler [1]
-	t_cell = getcell(t_row, t_col)
-	t_cell [2] = True
-	t_cell [3] = " "
-	
+	# remove the wall at the starting location
+	t_cell = getcell (tunneler)
+	removewall (t_cell)
+
+
 	# loop until the tunneler_history list size is 0
 	stack_empty = False
 	while stack_empty == False:
 
-		time.sleep(0.25)		# DEBUG-SLEEP
+		if SHOW_DEMO:
+			time.sleep(0.25)		# DEBUG-SLEEP
 
-		# clear the console
-		system("clear")	 	# DEBUG
+			# clear the console
+			system("clear")	 	# DEBUG
 
-		# change the color of the cell the tunneler is in
-		t = getcell(tunneler[0], tunneler[1])	 	# DEBUG
-		t [3] = "\u001b[31m" + wall_character + "\u001b[0m" 	# DEBUG
+			# change the color of the cell the tunneler is in
+			t = getcell (tunneler)	 	# DEBUG
+			setcharacter(t, "\u001b[31m" + WALL + "\u001b[0m")	# DEBUG
 
-		print_maze() 	# DEBUG
+			print_maze() 	# DEBUG
 
-		print ("\n             r   c")
-		print ("tunneler = [", tunneler[0], ",", tunneler[1], "]") 	# DEBUG
+			print ("\n             r   c")
+			print ("tunneler = [", tunneler[0], ",", tunneler[1], "]") 	# DEBUG
 
-		t [3] = "\u001b[0m " 	# DEBUG
-		
-		time.sleep (0.25)
+			t [3] = "\u001b[0m " 	# DEBUG
+			
 
 		# move the tunneler
 		tunneler = getunvisitedsurrounded(tunneler)
@@ -127,30 +142,32 @@ def tunnelthemaze():
 		# pop the stack
 		if tunneler == 0:
 			tunneler = tunneler_history.pop()
+
 		else:
 			tunneler_history.append(tunneler)
 
 			# mark cells tunneled through as visited
-			t_row = tunneler [0]
-			t_col = tunneler [1]
-			t_cell = getcell(t_row, t_col)
-			t_cell [2] = True
-			t_cell [3] = " "
+			t_cell = getcell (tunneler)
+			removewall (t_cell)
 
+			t_row = getrow (t_cell)
+			t_col = getcol (t_cell)
+
+			# determine which cell is behind the tunneler
 			if direct == UP: 
-				behind_cell = getcell(t_row + 1, t_col)
-			elif direct == DOWN:
-				behind_cell = getcell(t_row - 1, t_col)
-			elif direct == LEFT:
-				behind_cell = getcell(t_row, t_col + 1)
-			elif direct == RIGHT:
-				behind_cell = getcell(t_row, t_col - 1)
-			else:
-				behind_cell = None
+				behind_cell = getcell ( [t_row+1, t_col] )
 
-			if behind_cell != None:
-				behind_cell [2] = True
-				behind_cell [3] = " "
+			elif direct == DOWN:
+				behind_cell = getcell ( [t_row-1, t_col] )
+
+			elif direct == LEFT:
+				behind_cell = getcell ( [t_row, t_col+1] )
+
+			elif direct == RIGHT:
+				behind_cell = getcell ( [t_row, t_col-1] )
+
+			# remove the wall behind the tunneler
+			removewall (behind_cell)
 
 
 		# check for empty stack
@@ -163,13 +180,13 @@ def tunnelthemaze():
 
 # function to check for unvisited cells
 # RETURNS 
-#	a getcell position -> [getrow, getcol]
+#	a cell position -> [getrow, getcol]
 # 	OR 
 #	0 if no unvisited surrounding cells
 def getunvisitedsurrounded(cellpos):
 
-	# temporary cell
-	checkcell = []
+	# temporary cell used for checking
+	tocell = []
 
 	# loop 4 times (1 for each direction)
 	for d in range(4):
@@ -181,7 +198,7 @@ def getunvisitedsurrounded(cellpos):
 			# increment direction but reset to 0 if > 4
 			direction = (direction + 1) % 4
 		
-		# print ("\nchecking direction", getdirection(direction) )  # DEBUG
+		# print ("\nchecking direction", numtodirection(direction) )  # DEBUG
 
 		# set the getrow and column of the cell to be checked
 		if 	 direction == UP:
@@ -243,20 +260,21 @@ def getunvisitedsurrounded(cellpos):
 			
 		
 		# copy the cells
-		checkcell = getcell (r, c)
-		startcell = getcell(cellpos [0], cellpos [1])
+		to_pos = [r, c]
+		tocell = getcell (to_pos)
+		fromcell = getcell(cellpos)
 
 
 		# check for visited status
-		# print ("tocell =", checkcell) 	# DEBUG
-		# print ("fromcell =", startcell ) 	# DEBUG
+		# print ("tocell =", tocell) 	# DEBUG
+		# print ("fromcell =", fromcell ) 	# DEBUG
 		if (
-			checkcell [2] == False 
+			getvisited (tocell) == False 
 			and 
-			checkcell != startcell
+			tocell != fromcell
 		):
 			# print ("unvisited")		# DEBUG
-			return [ checkcell, direction ]
+			return [ tocell, direction ]
 		
 		# print ("visited")	# DEBUG
 
@@ -277,67 +295,63 @@ def createexit():
 		col = random.randint( 1, width - 2 )
 		
 		# get cells
-		inner = getcell(1, col)
-		exit = getcell (0, col)
+		inner = getcell ( [1, col] )
+		exit = getcell ( [0, col] )
 
 		# check visited
-		if inner [2] and not exit[2]:
-			exit [2] = True
-			exit [3] = " "
+		if getvisited (inner)  and  not getvisited (exit):
+			removewall (exit)
 			break
 
 	# Bottom
-	for col in range(1, width - 2):
+	for col in range(1, width-2):
 
 		if edge != 1:
 			break
 
-		col = random.randint( 1, width - 2 )
+		col = random.randint( 1, width-2 )
 		
 		# get cells
-		inner = getcell (height - 2, col)
-		exit = getcell (height - 1, col)
+		inner = getcell ( [height-2, col] )
+		exit  = getcell ( [height-1, col] )
 
 		# check visited
-		if inner [2] and not exit[2]:
-			exit [2] = True
-			exit [3] = " "
+		if getvisited (inner)  and  not getvisited (exit):
+			removewall (exit)
 			break
 
 	# Left
-	for row in range(1, height - 2):
+	for row in range(1, height-2):
 
 		if edge != 2:
 			break
 
-		row = random.randint( 1, height - 2 )
+		row = random.randint( 1, height-2 )
 		
 		# get cells
-		inner = getcell(row, 1)
-		exit = getcell (row, 0)
+		inner = getcell ( [row, 1] )
+		exit  = getcell ( [row, 0] )
 
 		# check visited
-		if inner [2] and not exit[2]:
-			exit [2] = True
-			exit [3] = " "
+		if getvisited (inner)  and  not getvisited (exit):
+			removewall (exit)
 			break
 
 	# Right
-	for row in range(1, height - 2):
+	for row in range(1, height-2):
 
 		if edge != 3:
 			break
 
-		row = random.randint( 1, height - 2 )
+		row = random.randint( 1, height-2 )
 		
 		# get cells
-		inner = getcell(row, width - 2)
-		exit = getcell (row, width - 1)
+		inner = getcell ( [row, width-2] )
+		exit  = getcell ( [row, width-1] )
 
 		# check visited
-		if inner [2] and not exit[2]:
-			exit [2] = True
-			exit [3] = " "
+		if getvisited (inner)  and  not getvisited (exit):
+			removewall (exit)
 			break
 
 
@@ -355,7 +369,7 @@ def print_maze():
 	# loop through each getcell in the maze
 	for row in range(height):
 		for col in range (width):
-			c = getcell(row, col)
+			c = getcell( [row, col] )
 			
 			print ( getcharacter (c), sep="",  end="")
 
